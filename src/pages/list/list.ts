f0ae0033f269e6f0d77cb39fc4eb73e11183a7ca
Arthur César <arthcesar@hotmail.com>
2019-01-03 23:08:11 -0300

    Start Simple Deep Link Project Ionic

---
+++ src/pages/list/list.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{id: number, title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public socialSharing: SocialSharing) {
+    this.selectedItem = navParams.get('item');
+
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        id: i,
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    this.navCtrl.push(ListPage, {
+      'listID': item.id
+    });
+  }
+
+  shareItem(item) {
+    this.socialSharing.share("Check this item:  demoapp://list/lists/" + item.id, item.title, item.note, item.icon)
+    .then(() => {
+
+    })
+    .catch(() => {
+
+    });
+  }
+}
